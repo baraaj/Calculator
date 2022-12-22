4a6dc16838808ab5f2aad9b681b86c863259c9de
baraaj <jeridibaraa@gmail.com>
2022-12-22 20:28:05 +0100

    last commit

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -3,17 +3,23 @@ import Button from '../Button/Button';
 import ButtonBox from '../Button/ButtonBox';
 import Screen from '../Screen/Screen';
 import Wrapper from '../Wrapper/Wrapper';
-import {Line} from 'react-chartjs-2';
+import { Line } from "react-chartjs-2";
 import  { useState } from "react";
- 
+import { Chart, ChartConfiguration, LineController, CategoryScale,LineElement, PointElement, LinearScale, Title} from 'chart.js' 
+
+Chart.register(CategoryScale,LinearScale,PointElement,LineElement);
  
 
 export default function Graph() {
   let [x,setX]=useState([]);
   let [y,setY]=useState([]);
   let [ch,setCh]=useState("");
+  
   const options = {
+    
     responsive: true,
+    
+  type: 'line',
     plugins: {
        
       title: {
@@ -22,14 +28,16 @@ export default function Graph() {
       },
     },
   };
-  const data = {
+  
+ const data = {
       labels: x,
       datasets: [{
       label: 'My Graph',
       data: y,
       fill: false,
       borderColor: 'rgb(75, 192, 192)',
-      tension: 0.1
+      tension: 0.1,
+      stacked: false,
     }]
   };
   const btnValues = [
@@ -60,7 +68,7 @@ export default function Graph() {
                   else if(btn==="="){
                      setX([0,1,2,3,4,5,6,7,8,9,10]);
                      setY(x.map(i=>Function(`'use strict'; return (${ch.replaceAll("X",i.toString())})`)()))
-                    
+                     console.log(y)
                      
                   }
                    
@@ -76,7 +84,7 @@ export default function Graph() {
         }
       </ButtonBox>
   </Wrapper>
-  <Line options={options} data={data} />
+  <Line data={data} options={options} />
   </>
 
   )
